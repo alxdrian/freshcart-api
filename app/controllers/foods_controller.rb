fabf9525ae7140c3e5569aaae4e9b448a0477496
alxdrian <alex_97piscis@outlook.com>
2021-11-22 23:54:03 -0500

    add images to json response for food controllers

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,20 @@
 class FoodsController < ApplicationController
+    before_action :set_food, only: [:show]
+
     def index
         @foods = Food.all
-        render json: @foods
+        render json: @foods.map { |food|
+          food.as_json.merge({ image: url_for(food.image) })
+        }
+    end
+
+    def show
+        render json: @food.as_json.merge({ image: url_for(@food.image) })
+    end
+
+    private
+
+    def set_food
+        @food = Food.find(params[:id])
     end
 end
